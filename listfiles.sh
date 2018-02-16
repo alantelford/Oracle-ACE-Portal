6efb787aeb6c6f0a4ebd3bbb2ea4508801e72639
alan.telford <alan.telford@woodgroup.com>
2018-02-16 15:22:07 +0000

    added in  listfiles.sh

--- listfiles.sh
+++ listfiles.sh
@@ -35,14 +35,34 @@
 # Value 6
 
 #constant variables
+JAVA_PATH=$(echo "${PWD//$JAVA_TOP/}")
+# remove first char of JAVA_PATH
+JAVA_PATH=${JAVA_PATH#?}
+
+# APPLICATION CODE DEFAULT
 APPL_CODE=xbol
+# Delimiter character
 DELIM=,
+# Language Code
+LANG=US
+DPATH=DEPLOYMENT_PATH
+LOBTYPE=LOB_TYPE
 OBJECT_TYPE=""
-FILENAME=""
+PARAMETER_LIST=""
+TEMPDIR=oaf_file_zip
+
+if [ ! -d $TEMPDIR ]
+then
+ mkdir $TEMPDIR
+fi 
+
+rm ./$TEMPDIR/*
+
 
 #function to derive the objct type
 get_obj_type() {
  EXTENSION=${FILENAME#*.}     # Right of .
+ PARAMETER_LIST=${DELIM}${DPATH}${DELIM}${FILEPATH}
 
  OBJECT_TYPE=""
 
@@ -59,7 +79,9 @@ get_obj_type() {
   xml)
     case $FILENAME in
     *RN.xml)
-    OBJECT_TYPE=oaf_page
+    ;;
+    server.xml)
+    OBJECT_TYPE=gen_cp
     ;;
     *PG.xml)
     OBJECT_TYPE=oaf_page
@@ -89,10 +111,25 @@ do
  if [ ! -d "$FILE"  ]
  then
   FILENAME=$(basename $FILE)
-  
+  FILEPATH=$(dirname $FILE)  
+  #trim the first char off the path
+  FILEPATH=${FILEPATH#?}
+  # tag onto java path 
+  FILEPATH=${JAVA_PATH}${FILEPATH}
   get_obj_type
+  cp $FILE $TEMPDIR
 
-  echo  "${FILENAME}${DELIM}${APPL_CODE}${DELIM}${FILETAG}${DELIM}${OBJECT_TYPE}"
+  echo  "${FILENAME}${DELIM}${APPL_CODE}${DELIM}${FILETAG}${DELIM}${OBJECT_TYPE}${DELIM}${LANG}${DELIM}0${PARAMETER_LIST}" >>$TEMPDIR/manifest.csv
  fi
 
 done
+ZIPFILE=oaf_file_${FILETAG}.zip
+cat $TEMPDIR/manifest.csv 
+cd $TEMPDIR
+zip $ZIPFILE *
+cp $ZIPFILE $INTERFACE_HOME/outgoing 
+cd ..
+echo "Copied zip file ${ZIPFILE}.zip to outgoing"
+
+
+
